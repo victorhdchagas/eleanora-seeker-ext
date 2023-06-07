2303a3c1f274b7861d04ae6adadab028cb726095
victorhdchagas <victorhdchagas@live.com>
2023-06-07 19:21:49 -0300

    First commit

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+
+function mountParam(url){
+    return `${url.protocol}://${url.hostname}${url.port?":"+url.port:""}${url.pathname}${url.search}`
+}
+
+function mountPostBody(urls){
+    return urls.map(url=>mountParam(url))
+}
+
+
+async function sendRequest(endpoint,urlList){
+    const response =await fetch(endpoint,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(urlList)
+    });
+    return response.status
+
+}
+
+chrome.tabs.onActivated.addListener(async ( {tabId})=>{
+    const sendingEndpoint = await chrome.storage.sync.get("url")
+    if(!sendingEndpoint || sendingEndpoint.url.length==0) return;
+    const allTabs = await chrome.tabs.query({active:true})
+    const activatedTab = allTabs.find(t=>t.id==tabId)
+    const listeningTabs = await chrome.tabs.query({audible:true})
+    console.log(sendingEndpoint.url)
+    if(activatedTab && activatedTab.url.length>0){
+        try {
+            const activateUrl = new URL(activatedTab.url)
+            console.log(listeningTabs)
+            const toSend = mountPostBody([activateUrl, ].concat(listeningTabs.map(t=>new URL(t.url))))
+            console.log(await sendRequest(sendingEndpoint.url,toSend))
+            
+        } catch (error) {
+            console.error("Non url supplied",error)
+        }
+    }
+
+})
---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import {useState, useEffect} from 'react';
+import './App.css'
+
+function App() {
+    const [url,setUrl] = useState("")
+    const [status,setStatus] = useState(false)
+
+    const checkServerHealth = async (url:string)=>{
+        try {
+            const urlInstance = new URL(url)
+    
+            const response = await fetch(urlInstance.origin+'/ping',{
+                method:"POST",
+            })
+            return response.status==200
+            
+        } catch (error) {
+            console.log('error happened code 201')
+        }
+        return false;
+    }
+
+    useEffect(()=>{
+        chrome.storage.sync.get((items)=>{
+            setUrl(items.url)
+            checkServerHealth(items.url)
+        })
+    },[])
+
+    const update= async (e:React.FormEvent)=>{
+        e.preventDefault()
+        chrome.storage.sync.set({url});
+        setStatus(await checkServerHealth(url))
+    }
+
+    return (
+        <form className={"w-full flex flex-col gap-2"} onClick={update}>
+            <h1 className="text-xl font-bold">
+                Eleanora Seeker 
+            </h1>
+
+            <h2 className="text-md ">
+                Insert eleanora endpoint
+            </h2>
+            <input type="url"
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-full-name"
+            placeholder='http://localhost:62236' onChange={(e)=>setUrl(e.target.value)} value={url}/>
+            <button type="submit"
+            className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-md  active:bg-blue-800"
+            value="Enviar">
+                Enviar
+            </button>
+            <span>Status: <span className={"ball ".concat(status?'b-ok':'b-error')}></span></span>
+        </form>
+    )
+}
+
+export default App
